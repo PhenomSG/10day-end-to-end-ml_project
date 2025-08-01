6c20408225a8c92823688e524bfaa9deeb31c630
Sahaj <sahajguptarocks@gmail.com>
2025-08-01 22:57:01 +0530

    fb

--- setup.py
+++ setup.py
@@ -4,6 +4,23 @@ It is a module used to build and distribute Python packages
 from conda_build.skeletons.pypi import get_requirements
 from setuptools import find_packages,setup
 
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path: str)->list[str]:
+    '''
+    this func will return the list of requiremnets
+
+    add -e. in requirements.txt to trigger setup.py
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
 setup(
 
     name='ml-proj',
